43b85f23e23e5f2c938cf04446022a49d2756270
Krzysztof Purgat <kr.purgat@gmail.com>
2022-08-12 16:18:44 +0200

    Add home page with menu

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,12 @@
+function Menu() {
+  return (
+    <div className="Menu">
+      <div className="container">
+        <h3 className="hello">Card Game</h3>
+        <button className="button">Play</button>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Menu from "../Components/Menu";
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
+
+export default Home;
